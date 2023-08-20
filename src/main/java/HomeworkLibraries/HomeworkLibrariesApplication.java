20373f548193687be758793ab80f21ba7ffd91a6
ValeriyKryatov <Kwaler@mail.ru>
2023-08-20 23:56:29 +0300

    HomeworkLibraries. First commit

---
+++ src/main/java/HomeworkLibraries/HomeworkLibrariesApplication.java
@@ -0,0 +1,12 @@
+package HomeworkLibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkLibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkLibrariesApplication.class, args);
+	}
+}
